4f6116db7a308e4f1ee6df9ed50efde9fe4f6ee9
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-11-09 20:50:27 -0300

    update

--- atividade_dom/atividade1.js
+++ atividade_dom/atividade1.js
@@ -12,3 +12,13 @@ function mostrarOla(){
     let inputNome = document.querySelector('#nome').value;
     document.querySelector('#msg').textContent = 'olá '+inputNome;
 }
+
+function mostrarOla2(){
+    let inputNome = document.querySelector('#nome').value;
+    document.querySelector('#msg2').textContent = 'olá '+inputNome;
+}
+
+let botao = document.querySelector('#botao');
+botao.addEventListener('click', function(){
+    mostrarOla2()
+});
