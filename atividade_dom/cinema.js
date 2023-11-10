08225a35e94393bad3a32b0f628c572738cbaffb
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-11-09 21:30:28 -0300

    update

--- atividade_dom/cinema.js
+++ atividade_dom/cinema.js
@@ -2,13 +2,13 @@ let frm = document.querySelector("form")
 let resp1 = document.querySelector("h3")
 let resp2 = document.querySelector("h4")
 
-frm.addEventListener("submit",  function() {
+frm.addEventListener('submit',  function() {
     let nome = frm.inNome.value;
-    let tempo = frm.inTempo.value;
+    let tempo = Number(frm.inTempo.value);
 
     let horas = Math.floor( tempo /60);
     let minutos = tempo % 60;
 
-    resp1.innerText = nome;
-    resp2.innerText = (horas + "hora "+ minutos + " minutos");
+    resp1.innerText = `título: ${nome}`;
+    resp2.innerText = `duração: ${horas} hora(s) e ${minutos} min.`;
 })
